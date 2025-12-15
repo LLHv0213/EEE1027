e06f9363b2fcba0b59ac713567133cbb6932be7b
LLHv0213 <leelithang@gmail.com>
2025-12-15 20:04:35 +0800

    Week 3 challenge 1 (PID)

--- Week_3_challenge_1_PID.ino
+++ Week_3_challenge_1_PID.ino
@@ -3,42 +3,58 @@
 // ================= LCD =================
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
-// ================= IR Sensors =================
+// ================= IR SENSORS =================
 const int IR_LEFT   = A5;
 const int IR_CENTER = A1;
 const int IR_RIGHT  = A4;
 
-// ================= Motors =================
-const int IN1 = 1;    // Right motor
+// ================= MOTOR PINS =================
+const int IN1 = 1;     // Right motor
 const int IN2 = 2;
-const int IN3 = 12;   // Left motor
+const int IN3 = 12;    // Left motor
 const int IN4 = 13;
-const int ENA = 11;   // Left PWM
-const int ENB = 3;    // Right PWM
+const int ENA = 11;    // Left PWM
+const int ENB = 3;     // Right PWM
 
-// ================= PID Parameters =================
-float Kp = 25.0;
-float Ki = 0.0;
-float Kd = 12.0;
+// ================= ENCODERS =================
+const int ENC_LEFT  = A3;
+const int ENC_RIGHT = A2;
+long leftCount = 0;
+long rightCount = 0;
+int lastLeftState = LOW;
+int lastRightState = LOW;
+
+// ================= WHEEL PARAMETERS =================
+const float wheelDiameter = 6.5;
+const int pulsesPerRev = 20;
+const float cmPerPulse = 3.1416 * wheelDiameter / pulsesPerRev;
+
+// ================= PID PARAMETERS =================
+float Kp = 18.5;
+float Ki = 0.05;
+float Kd = 11.5;
 
 float error = 0;
 float lastError = 0;
 float integral = 0;
 
-// ================= Motion =================
 int baseSpeed = 160;
 
-// ================= Timing =================
+// ================= FLAGS =================
+bool sharpTurning = false;
+bool sharpRecovery = false;
+int sharpTurnDirection = 0;
+
+// ================= DISTANCE & TIME =================
 unsigned long startTime;
+bool reached40 = false;
+bool stoppingNow = false;
+unsigned long stopStartTime = 0;
 
-// ================= Motor Functions =================
-void movePID(int leftSpeed, int rightSpeed) {
-  leftSpeed  = constrain(leftSpeed,  0, 255);
-  rightSpeed = constrain(rightSpeed, 0, 255);
-
-  analogWrite(ENA, leftSpeed);
-  analogWrite(ENB, rightSpeed);
-
+// ================= MOTOR FUNCTIONS =================
+void movePID(int leftPWM, int rightPWM) {
+  analogWrite(ENA, leftPWM);
+  analogWrite(ENB, rightPWM);
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, HIGH);
   digitalWrite(IN3, LOW);
@@ -54,6 +70,18 @@ void stopMotors() {
   digitalWrite(IN4, LOW);
 }
 
+// ================= ENCODER READ =================
+void readEncoders() {
+  int l = digitalRead(ENC_LEFT);
+  int r = digitalRead(ENC_RIGHT);
+
+  if (lastLeftState == LOW && l == HIGH) leftCount++;
+  if (lastRightState == LOW && r == HIGH) rightCount++;
+
+  lastLeftState = l;
+  lastRightState = r;
+}
+
 // ================= SETUP =================
 void setup() {
   lcd.begin(16, 2);
@@ -62,63 +90,96 @@ void setup() {
   pinMode(IN3, OUTPUT); pinMode(IN4, OUTPUT);
   pinMode(ENA, OUTPUT); pinMode(ENB, OUTPUT);
 
-  lcd.print("PID Line Follower");
-  delay(1000);
-  lcd.clear();
+  pinMode(ENC_LEFT, INPUT_PULLUP);
+  pinMode(ENC_RIGHT, INPUT_PULLUP);
+
+  lastLeftState = digitalRead(ENC_LEFT);
+  lastRightState = digitalRead(ENC_RIGHT);
 
   startTime = millis();
+
+  lcd.print("PID Line Tracker");
+  delay(800);
+  lcd.clear();
 }
 
 // ================= LOOP =================
 void loop() {
-  // ----- Read sensors -----
-  int L = analogRead(IR_LEFT);
-  int C = analogRead(IR_CENTER);
-  int R = analogRead(IR_RIGHT);
+  // ----- Read IR sensors -----
+  int L = constrain(map(analogRead(IR_LEFT),   0, 1023, 0, 10), 0, 10);
+  int C = constrain(map(analogRead(IR_CENTER), 0, 1023, 0, 10), 0, 10);
+  int R = constrain(map(analogRead(IR_RIGHT),  0, 1023, 0, 10), 0, 10);
 
-  // ----- Convert to black / white -----
-  int Lb = (L < 400);
-  int Cb = (C < 400);
-  int Rb = (R < 400);
+  bool leftBlack   = (L <= 3);
+  bool rightBlack  = (R <= 3);
+  bool centerBlack = (C >= 6);
 
-  // ----- End condition -----
-  if (Lb && Cb && Rb) {
+  readEncoders();
+
+  float distanceCM = ((leftCount + rightCount) / 2.0) * cmPerPulse;
+
+  // ----- 40 CM STOP -----
+  if (!reached40 && distanceCM >= 40.0) {
+    reached40 = true;
+    stoppingNow = true;
+    stopStartTime = millis();
     stopMotors();
-    lcd.clear();
+  }
+
+  if (stoppingNow) {
+    stopMotors();
     lcd.setCursor(0,0);
-    lcd.print("END OF TRACK");
-    lcd.setCursor(0,1);
     lcd.print("Time:");
     lcd.print((millis() - startTime)/1000);
-    lcd.print("s");
+    lcd.print("s   ");
+
+    lcd.setCursor(0,1);
+    lcd.print("Dist:");
+    lcd.print(distanceCM,1);
+    lcd.print("cm  ");
+
+    if (millis() - stopStartTime >= 3000) stoppingNow = false;
+    else return;
+  }
+
+  // ----- DISPLAY -----
+  lcd.setCursor(0,0);
+  lcd.print("Time:");
+  lcd.print((millis() - startTime)/1000);
+  lcd.print("s   ");
+
+  lcd.setCursor(0,1);
+  lcd.print("Dist:");
+  lcd.print(distanceCM,1);
+  lcd.print("cm  ");
+
+  // ----- ALL BLACK STOP -----
+  if (leftBlack && centerBlack && rightBlack) {
+    stopMotors();
     while(true);
   }
 
-  // ----- Calculate PID error -----
-  error = (-1 * Lb) + (0 * Cb) + (1 * Rb);
+  // ================= PID ERROR =================
+  if (centerBlack && !leftBlack && !rightBlack) error = 0;
+  else if (leftBlack && !centerBlack) error = -1;
+  else if (rightBlack && !centerBlack) error = 1;
+  else if (leftBlack && centerBlack) error = -2;
+  else if (rightBlack && centerBlack) error = 2;
+  else error = lastError;
 
+  // ================= PID COMPUTATION =================
   integral += error;
   float derivative = error - lastError;
-
-  float correction = Kp * error + Ki * integral + Kd * derivative;
-
-  int leftSpeed  = baseSpeed - correction;
-  int rightSpeed = baseSpeed + correction;
-
-  movePID(leftSpeed, rightSpeed);
-
+  float correction = Kp*error + Ki*integral + Kd*derivative;
   lastError = error;
 
-  // ----- LCD Display -----
-  lcd.setCursor(0,0);
-  lcd.print("Err:");
-  lcd.print(error);
-  lcd.print("       ");
+  int leftPWM  = baseSpeed - correction;
+  int rightPWM = baseSpeed + correction;
 
-  lcd.setCursor(0,1);
-  lcd.print("T:");
-  lcd.print((millis() - startTime)/1000);
-  lcd.print("s     ");
+  leftPWM  = constrain(leftPWM, 0, 255);
+  rightPWM = constrain(rightPWM, 0, 255);
 
-  delay(20);
+  // ================= MOVE =================
+  movePID(leftPWM, rightPWM);
+  delay(15);
 }
